0ec28d613f149388c838f406edd87a2575b9475f
ToastXC <100072983+toastxc@users.noreply.github.com>
2025-10-14 11:46:43 +0800

    Initial commit

---
+++ src/backend.rs
@@ -0,0 +1,98 @@
+use dioxus::{logger::tracing::warn, prelude::*};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Root {
+    pub result: Results,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Results {
+    pub uv: f32,
+    #[serde(rename = "uv_time")]
+    pub uv_time: String,
+    #[serde(rename = "uv_max")]
+    pub uv_max: f64,
+    #[serde(rename = "uv_max_time")]
+    pub uv_max_time: String,
+    pub ozone: f64,
+    // #[serde(rename = "ozone_time")]
+    // pub ozone_time: String,
+    // #[serde(rename = "safe_exposure_time")]
+    // pub safe_exposure_time: SafeExposureTime,
+    // #[serde(rename = "sun_info")]
+    // pub sun_info: SunInfo,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SafeExposureTime {
+    pub st1: Value,
+    pub st2: Value,
+    pub st3: Value,
+    pub st4: Value,
+    pub st5: Value,
+    pub st6: Value,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SunInfo {
+    #[serde(rename = "sun_times")]
+    pub sun_times: SunTimes,
+    #[serde(rename = "sun_position")]
+    pub sun_position: SunPosition,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SunTimes {
+    pub solar_noon: String,
+    pub nadir: String,
+    pub sunrise: String,
+    pub sunset: String,
+    pub sunrise_end: String,
+    pub sunset_start: String,
+    pub dawn: String,
+    pub dusk: String,
+    pub nautical_dawn: String,
+    pub nautical_dusk: String,
+    pub night_end: String,
+    pub night: String,
+    pub golden_hour_end: String,
+    pub golden_hour: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SunPosition {
+    pub azimuth: f64,
+    pub altitude: f64,
+}
+
+#[cfg(feature = "server")]
+use lazy_static::lazy_static;
+#[cfg(feature = "server")]
+lazy_static! {
+    static ref TOKEN: String = String::from_utf8(std::fs::read("token.txt").unwrap()).unwrap();
+}
+
+#[server]
+pub async fn weather_get() -> Result<Root, ServerFnError> {
+    let url = format!("https://api.openuv.io/api/v1/uv?lat=-32.056946&lng=115.743889");
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .header("x-access-token", TOKEN.to_string())
+        .send()
+        .await
+        .unwrap()
+        .json()
+        .await
+        .unwrap();
+    Ok(response)
+}
---
+++ src/main.rs
@@ -0,0 +1,76 @@
+pub mod backend;
+use dioxus::{logger::tracing::warn, prelude::*};
+
+use crate::backend::weather_get;
+
+const MAIN_CSS: Asset = asset!("/assets/ebook.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        Echo {}
+    }
+}
+#[component]
+fn Echo() -> Element {
+    let Some(weather) = use_resource(move || async move { weather_get().await.unwrap() })
+        .read_unchecked()
+        .clone()
+    else {
+        return rsx! {
+            h1 { "Server Error :/" }
+        };
+    };
+
+    let mut weather = use_signal(|| weather);
+
+    let (current, max) = (
+        format!("{:.1}", weather.read().result.uv),
+        format!("{:.1}", weather.read().result.uv_max),
+    );
+
+    let mut is_loading = use_signal(|| false);
+
+    rsx! {
+
+
+        div { class: "boxes",
+            h1 { class: "box", "UV Index for Fremantle" }
+        }
+        br {}
+
+        div { class: "boxes",
+
+            if !*is_loading.read() {
+                h2 { class: "box bx-line", "Max: {max}" }
+
+                h2 { class: "box bx-line", "Current: {current}" }
+            } else {
+                h2 { class: "box bx-line", ". . . " }
+
+                h2 { class: "box bx-line", ". . ." }
+            }
+        }
+
+        div { class: "boxes",
+            a {
+                class: "box bx-line",
+                onclick: move |_| async move {
+                    is_loading.set(true);
+                    let uv = weather_get().await.unwrap();
+                    weather.set(uv);
+                    warn!("YIPEEE");
+                    is_loading.set(false);
+                },
+
+
+                "Refresh"
+            }
+        }
+    }
+}
